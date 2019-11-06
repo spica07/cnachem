13b5c3944f5890ddf2eff3d72048db9b0e238e07
cherrylime <cherrylime78@gmail.com>
2019-11-06 14:29:45 +0900

    indigo 오류나네..

--- src/main/java/com/example/demo/service/SampleService.java
+++ src/main/java/com/example/demo/service/SampleService.java
@@ -25,28 +25,31 @@ public class SampleService {
 		
 //		srcSmile = "C1=CN=CC=C1";  //검색구조 1
 //		/sample/smiles/filter?key=N1=CC=CN1
-		
-		
 //		srcSmile = "N1=CC=CN1"; // 검색구조 2
 //      /sample/smiles/filter?key=N1=CC=CN1
 		
 		
-		
-		Indigo indigo = new Indigo();
 		List <LinkedHashMap<String,Object>> smileArray = new ArrayList<LinkedHashMap<String,Object>>(); 
 		
-	    // 분자식 조회조건 Object 생성
-	   	IndigoObject query = indigo.loadQueryMolecule(srcSmile);
-	   	query.aromatize();
-	   	for(LinkedHashMap<String,Object> tgtSmile :  retrieveSmileData()) {
-	   		IndigoObject target = indigo.loadMolecule(tgtSmile.get("smiledata").toString());
-	   		IndigoObject matcher = indigo.substructureMatcher(target);
-	   		
-	   		IndigoObject find = matcher.match(query);
-	   		if(find != null) {
-	   			smileArray.add(tgtSmile);
-			}	
-	   	}
+		try {
+			Indigo indigo = new Indigo();
+		    // 분자식 조회조건 Object 생성
+		   	IndigoObject query = indigo.loadQueryMolecule(srcSmile);
+		   	query.aromatize();
+		   	for(LinkedHashMap<String,Object> tgtSmile :  retrieveSmileData()) {
+		   		try {
+			   		IndigoObject target = indigo.loadMolecule(tgtSmile.get("smiledata").toString());
+			   		IndigoObject matcher = indigo.substructureMatcher(target);
+			   		
+			   		IndigoObject find = matcher.match(query);
+			   		if(find != null) {
+			   			smileArray.add(tgtSmile);
+					}	
+		   		}catch(Exception ex) {}
+		   	}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		return smileArray;
 	}
